534bc16b3c7ecb923bd94cd2423dd51098447bf0
Jamie Snape <jamie.snape@kitware.com>
2016-12-22 15:01:33 -0500

    Add pyqt formula

---
+++ pyqt.rb
@@ -0,0 +1,116 @@
+# Copyright 2009-2016 Homebrew contributors.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+class Pyqt < Formula
+  desc "Python bindings for Qt"
+  homepage "https://www.riverbankcomputing.com/software/pyqt/intro"
+  url "https://downloads.sf.net/project/pyqt/PyQt4/PyQt-4.11.4/PyQt-mac-gpl-4.11.4.tar.gz"
+  sha256 "f178ba12a814191df8e9f87fb95c11084a0addc827604f1a18a82944225ed918"
+
+  option "without-python", "Build without python 2 support"
+  depends_on :python3 => :optional
+
+  if build.without?("python3") && build.without?("python")
+    odie "pyqt: --with-python3 must be specified when using --without-python"
+  end
+
+  if MacOS.version >= :sierra
+    depends_on "cartr/qt4/qt"
+  else
+    depends_on "qt"
+  end
+
+  if build.with? "python3"
+    depends_on "sip" => "with-python3"
+  else
+    depends_on "sip"
+  end
+
+  def install
+    # On Mavericks we want to target libc++, this requires a non default qt makespec
+    if ENV.compiler == :clang && MacOS.version >= :mavericks
+      ENV.append "QMAKESPEC", "unsupported/macx-clang-libc++"
+    end
+
+    Language::Python.each_python(build) do |python, version|
+      ENV.append_path "PYTHONPATH", "#{Formula["sip"].opt_lib}/python#{version}/site-packages"
+
+      args = %W[
+        --confirm-license
+        --bindir=#{bin}
+        --destdir=#{lib}/python#{version}/site-packages
+        --sipdir=#{share}/sip
+      ]
+
+      # We need to run "configure.py" so that pyqtconfig.py is generated, which
+      # is needed by QGIS, PyQWT (and many other PyQt interoperable
+      # implementations such as the ROS GUI libs). This file is currently needed
+      # for generating build files appropriate for the qmake spec that was used
+      # to build Qt. The alternatives provided by configure-ng.py is not
+      # sufficient to replace pyqtconfig.py yet (see
+      # https://github.com/qgis/QGIS/pull/1508). Using configure.py is
+      # deprecated and will be removed with SIP v5, so we do the actual compile
+      # using the newer configure-ng.py as recommended. In order not to
+      # interfere with the build using configure-ng.py, we run configure.py in a
+      # temporary directory and only retain the pyqtconfig.py from that.
+
+      require "tmpdir"
+      dir = Dir.mktmpdir
+      begin
+        cp_r(Dir.glob("*"), dir)
+        cd dir do
+          system python, "configure.py", *args
+          inreplace "pyqtconfig.py", Formula["qt"].prefix, Formula["qt"].opt_prefix
+          (lib/"python#{version}/site-packages/PyQt4").install "pyqtconfig.py"
+        end
+      ensure
+        remove_entry_secure dir
+      end
+
+      # On Mavericks we want to target libc++, this requires a non default qt makespec
+      if ENV.compiler == :clang && MacOS.version >= :mavericks
+        args << "--spec" << "unsupported/macx-clang-libc++"
+      end
+
+      system python, "configure-ng.py", *args
+      system "make"
+      system "make", "install"
+      system "make", "clean" # for when building against multiple Pythons
+    end
+  end
+
+  def caveats
+    "Phonon support is broken."
+  end
+
+  test do
+    Pathname("test.py").write <<-EOS.undent
+      from PyQt4 import QtNetwork
+      QtNetwork.QNetworkAccessManager().networkAccessible()
+    EOS
+
+    Language::Python.each_python(build) do |python, _version|
+      system python, "test.py"
+    end
+  end
+end
